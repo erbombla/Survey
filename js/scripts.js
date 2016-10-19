498930fe0bb2d7fb11f17827fd392a31ddf5e846
Eric Bomblatus and Michelle Llaguno <eric@>
2016-10-19 16:14:50 -0700

    create an entire survey of favorite things while practicing forms and input types.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var nameInput = $("input#name").val();
+
+    var dob = $("#born").val();
+
+    var country = $("#country").val();
+
+    var beverage = $("input:radio[name=beverage]:checked").val();
+
+    var favoriteColor = $("#color").val();
+
+    var food = $("input:radio[name=food]:checked").val();
+});
